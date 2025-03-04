80713650b25ecc9249814dbe4e7869ba31a0775c
ArunkumarBikki <arunchintu0511@gmail.com>
2025-03-04 15:50:28 +0530

    python

---
+++ 01-03-25.py
@@ -0,0 +1,12 @@
+# Time and space complexities 
+
+# sum of natural numbers
+# num = int(input("Enter a number"))
+# sum = 0
+# for i in range(1,num+1):
+#     sum = sum + i
+#     print(sum)
+#        or 
+# print(num*(num+1)/2)
+
+# 0(1) < 0(logn) < 0(N) < 0(nlogn) < 0(n square) < 0(n cube)
